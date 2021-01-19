750d531ee0ab1bdd7d002fe2f863388a17bf0466
SeoJin Kim <sjinjin7@gmail.com>
2021-01-20 00:51:10 +0900

    BCryptePasswordEncoder 적용(회원가입, 로그인)
    * security 기본 설정
    1. pom.xml
    2. security-context.xml 추가
    3. BCryptPasswordEncoder bean 등록
    4. web.xml에서 security-context.xml 등록
    
    * 회원가입 적용
    1. 의존성 주입
    2. 변수 선언
    3. rawPw 대입
    4. encdoe()
    5. 인코딩한 비밀번호 member 객체 담기
    
    
    * 로그인 적용
    1. MemberMapper.xml memberlogin 수정
    	memberpw도 반환되도록 수정
    1. Controller loginPOST() 수정
    	1. 기존 코드 주석
    	2. return null 작성
    	3. session 변수, rawPw, encdoePw 선언
    	4. memberlogin() 로그인 쿼리 실행
    	5. if문 작성	아이디 존재유무 판단
    		false	// 로그인 실패(존재아이디 x)
    			1. addFlashAttribute result에 0 담기.
    			2. 리다이렉트 로그인페이지
    		true	// 아이디 존재
    			1. rawPw, encdoePw 대입
    			2. if문 작성	matches()통해 비밀번호 확인
    				false	//로그인 실패(비밀번호 x)
    				1. addFlashAttribute result에 0 담기.
    				2. 리다이렉트 로그인페이지
    				true	//비밀번호 일치
    				1. vo담긴 비밀번호 삭제
    				2. session member에 vo객체 담기
    				3. return 리다일게트 메인페이지

---
+++ VamPa/src/main/java/com/vam/config/Test.java
@@ -0,0 +1,5 @@
+package com.vam.config;
+
+public class Test {
+
+}
--- VamPa_MySQL/src/main/java/com/vam/controller/MemberController.java
+++ VamPa_MySQL/src/main/java/com/vam/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,6 +32,9 @@ public class MemberController {
 	
 	@Autowired
 	private JavaMailSender mailSender;	
+	
+	@Autowired
+	private BCryptPasswordEncoder pwEncoder;
 	
 	//회원가입 페이지 이동
 	@RequestMapping(value = "join", method = RequestMethod.GET)
@@ -54,13 +58,31 @@ public class MemberController {
 		
 		logger.info("join 진입");
 		
-		// 회원가입 서비스 실행
+		String rawPw = "";					// 인코딩 전 비밀번호
+		String encodePw = "";				// 인코딩 후 비밀번호
+		
+		rawPw = member.getMemberPw();
+		encodePw = pwEncoder.encode(rawPw);
+		member.setMemberPw(encodePw);
+		
+		/* 회원가입 쿼리 실행 */
 		memberservice.memberJoin(member);
 		
 		logger.info("join Service 성공");
 		
 		return "redirect:/main";
 		
+		
+		/* 기존 회원가입 코드
+		logger.info("join 진입");
+		
+		// 회원가입 서비스 실행
+		memberservice.memberJoin(member);
+		
+		logger.info("join Service 성공");
+		
+		return "redirect:/main";
+		*/
 	}
 	
 	// 아이디 중복 검사
@@ -136,6 +158,41 @@ public class MemberController {
 	public String loginPOST(HttpServletRequest request, MemberVO member, RedirectAttributes rttr)throws Exception{
 		
 		HttpSession session = request.getSession();
+		String rawPw = "";
+		String encodePw = "";
+		
+		MemberVO vo = memberservice.memberLogin(member);
+		
+		if(vo != null) {		// 아이디 존재 경우
+			rawPw = member.getMemberPw();
+			encodePw = vo.getMemberPw();
+			
+			if(true == pwEncoder.matches(rawPw, encodePw)) {
+				
+				vo.setMemberPw("");	// 비밀번호 비우기
+				
+				session.setAttribute("member", vo);
+				
+				return "redirect:/main";
+				
+			} else {			// 로그인 실패(비밀번호 일치하지 않을 경우)
+				
+				rttr.addFlashAttribute("result", 0);
+				return "redirect:/member/login";	// 로그인 페이지로 반환
+				
+			}
+			
+		} else {				// 로그인 실패(아이디 존재하지 않을 경우)
+			
+			rttr.addFlashAttribute("result", 0);
+			return "redirect:/member/login";	// 로그인 페이지로 반환
+			
+		}
+		
+		
+		
+	/*
+		HttpSession session = request.getSession();
 		MemberVO vo = memberservice.memberLogin(member);
 		System.out.println("" + vo);
 		
@@ -152,6 +209,7 @@ public class MemberController {
 		
 		
 		return "redirect:/main";
+	*/	
 	}	
 	
 	
