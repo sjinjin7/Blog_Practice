f185dd53bf9a6e3888de52c7f2f3603208f5a43d
SeoJin Kim <sjinjin7@gmail.com>
2021-02-06 08:00:08 +0900

    Service 어노테이션 누락 수정

--- VamPa_MySQL/src/main/java/com/vam/service/AuthorServiceImpl.java
+++ VamPa_MySQL/src/main/java/com/vam/service/AuthorServiceImpl.java
@@ -1,10 +1,12 @@
 package com.vam.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.vam.mapper.AuthorMapper;
 import com.vam.model.AuthorVO;
 
+@Service
 public class AuthorServiceImpl implements AuthorService {
 
 	@Autowired
