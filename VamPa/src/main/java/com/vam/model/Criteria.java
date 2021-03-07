0648b7933c7cc5b16327c232307a96e238c90345
SeoJin Kim <sjinjin7@gmail.com>
2021-03-08 00:02:16 +0900

    작가 목록 구현 1
    1. Criteria 작성
    	- MySQL
    		-skip(변수, 생성자 추가)
    		- setPage(), setAmount 수정
    2. AuthorVO	수정
    	- setAuthorid() 수정
    3. AuthorMapper.java
    	- authorGetList(), authorGetTotal()
    4. AuthorMapper.xml
    	- authorGetList(), authorGetTotal()
    5. AuthormapperTests.java
    	- authorGetList(), authorGetTotal()

---
+++ VamPa/src/main/java/com/vam/model/Criteria.java
@@ -0,0 +1,67 @@
+package com.vam.model;
+
+public class Criteria {
+
+	/* 현재 페이지 번호 */
+	private int pageNum;
+	
+	/* 페이지 표시 개수 */
+	private int amount;
+	
+	/* 검색 타입 */
+	private String type;
+	
+	/* 검색 키워드 */
+	private String keyword;
+	
+	/* Criteria 기본 생성자 */
+	public Criteria() {
+		this(1,10);
+	}
+	
+	/* Criteria 생성자 */
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+	
+	/* 검색 타입 데이터 배열 변환 */
+	public String[] getTypeArr() {
+		return type == null? new String[] {}: type.split("");
+	}
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	
+	
+}
--- VamPa_MySQL/src/test/java/com/vam/mapper/AuthorMapperTests.java
+++ VamPa_MySQL/src/test/java/com/vam/mapper/AuthorMapperTests.java
@@ -2,6 +2,8 @@ package com.vam.mapper;
 
 
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +11,20 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.vam.model.AuthorVO;
+import com.vam.model.Criteria;
+
+import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
 public class AuthorMapperTests {
 
 	@Autowired
 	private AuthorMapper mapper;
 	
 	/* 작가 등록 테스트 */
+	/*
 	@Test
 	public void authorEnroll() throws Exception{
 		
@@ -30,5 +37,35 @@ public class AuthorMapperTests {
 		mapper.authorEnroll(author);
 		
 	}
+	*/
+	
+	
+	/* 작가 목록 테스트 */
+	@Test
+	public void authorGetList() {
+		
+		Criteria cri = new Criteria();
+		
+		cri.setKeyword("엘런");
+		
+		List<AuthorVO> list = mapper.authorGetList(cri);
+		
+		list.forEach(a -> log.info(cri));
+		
+	}
+	
+	
+	/* 작가 수 테스트 */
+	@Test
+	public void authorGetTotal() {
+		
+		Criteria cri = new Criteria();
+		
+		cri.setKeyword("엘런");
+		
+		mapper.authorGetTotal(cri);
+		
+	}
+	
 	
 }
