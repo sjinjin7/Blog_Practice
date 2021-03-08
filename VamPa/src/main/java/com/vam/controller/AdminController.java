925f93fa50314b728c45f7601f86e93e4a36cad5
SeoJin Kim <sjinjin7@gmail.com>
2021-03-09 05:26:46 +0900

    작가 정보 수정 구현 - 3
    1. AdminController
    	- authorModifyPOST
    2.authorManage.jsp
    	- 수정 후 결과 경고창 기능 구현

--- VamPa/src/main/java/com/vam/controller/AdminController.java
+++ VamPa/src/main/java/com/vam/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -89,7 +90,7 @@ public class AdminController {
 	@GetMapping({"/authorDetail", "/authorModify"})
 	public void authorDetailGET(int authorId, Criteria cri, Model model) throws Exception {
 		
-		logger.info("authorDetail......." + cri);
+		logger.info("/authorDetail or /authorModify......." + cri);
 		
 		/* 목록 페이지 정보 */
 		model.addAttribute("cri", cri);
@@ -98,5 +99,19 @@ public class AdminController {
 		model.addAttribute("authorInfo", authorService.authorGetDetail(authorId));
 		
 	}
+	
+	/* 작가 정보 수정 */
+	@PostMapping("authorModify")
+	public String authorModifyPOST(AuthorVO author, RedirectAttributes rttr) throws Exception{
+		
+		logger.info("authorModifyPOST.........." + author);
+		
+		int result = authorService.authorModify(author);
+		
+		rttr.addFlashAttribute("modify_result", result);
+		
+		return "redirect:/admin/authorManage";
+	}
+
 	
 }
