3d6a84f3243c7b7f38bd1d64052fd989f61a56d9
SeoJin Kim <sjinjin7@gmail.com>
2021-03-18 03:58:54 +0900

    상품 등록 기능 구현 - 상품등록 Service
    			- AdminService 생성
    				- bookEnroll() 선언
    			- AdminServiceImpl 생성
    				- bookenroll() 구현

---
+++ VamPa_MySQL/src/test/java/com/vam/service/AdminServiceTests.java
@@ -0,0 +1,45 @@
+package com.vam.service;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.vam.model.BookVO;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class AdminServiceTests {
+
+	@Autowired
+	private AdminServiceImpl service;
+
+	/* 상품 등록 */
+	@Test
+	public void bookEnrollTest() throws Exception{
+		
+		BookVO book = new BookVO();
+		
+		book.setBookName("(service)mapper 테스트");
+		book.setAuthorId(123);
+		book.setPubleYear("2021-03-18");
+		book.setPublisher("출판사");
+		book.setCateCode("0231");
+		book.setBookPrice(20000);
+		book.setBookStock(300);
+		book.setBookDiscount(0.23);
+		book.setBookIntro("책 소개 ");
+		book.setBookContents("책 목차 ");
+		
+		log.info("book........" + book);
+		
+		service.bookEnroll(book);
+	}
+	
+}
