a10167c8a2a24b7c0a5d9354c2d969394f85f7b3
SeoJin Kim <sjinjin7@gmail.com>
2021-06-14 13:54:55 +0900

    트랜잭션 적용 및 테스트

--- VamPa_MySQL/src/test/java/com/vam/service/AdminServiceTests.java
+++ VamPa_MySQL/src/test/java/com/vam/service/AdminServiceTests.java
@@ -2,12 +2,16 @@ package com.vam.service;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.vam.model.AttachImageVO;
 import com.vam.model.BookVO;
 
 import lombok.extern.log4j.Log4j;
@@ -18,7 +22,7 @@ import lombok.extern.log4j.Log4j;
 public class AdminServiceTests {
 
 	@Autowired
-	private AdminServiceImpl service;
+	private AdminService service;
 
 	/* 상품 등록 */
 	/*
@@ -45,10 +49,57 @@ public class AdminServiceTests {
 	*/
 	
 	/* 카테고리 리스트 */
+	/*
 	@Test
 	public void cateListTest() throws Exception{
 		
 		log.info("cateList()..........." + service.cateList());
+		
+	}	
+	*/
+	
+	/* 상품 등록 & 상품 이미지 등록 테스트 */
+	@Test
+	public void bookEnrollTEsts() {
+
+		BookVO book = new BookVO();
+		// 상품 정보
+		book.setBookName("mapper 테스트");
+		book.setAuthorId(123);
+		book.setPubleYear("2021-03-18");
+		book.setPublisher("출판사");
+		book.setCateCode("202001");
+		book.setBookPrice(20000);
+		book.setBookStock(300);
+		book.setBookDiscount(0.23);
+		book.setBookIntro("책 소개 ");
+		book.setBookContents("책 목차 ");
+
+		// 이미지 정보
+		List<AttachImageVO> imageList = new ArrayList<AttachImageVO>(); 
+		
+		AttachImageVO image1 = new AttachImageVO();
+		AttachImageVO image2 = new AttachImageVO();
+		
+		image1.setFileName("test Image 1");
+		image1.setUploadPath("test image 1");
+		image1.setUuid("test1111");
+		
+		image2.setFileName("test Image 2");
+		image2.setUploadPath("test image 2");
+		image2.setUuid("test2222");
+		
+		imageList.add(image1);
+		imageList.add(image2);
+		
+		
+		book.setImageList(imageList);
+		
+		// bookEnroll() 메서드 호출
+		service.bookEnroll(book);
+		
+		System.out.println("등록된 VO : " + book);
+		
 		
 	}	
 	
