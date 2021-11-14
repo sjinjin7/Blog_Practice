b4c9a76b78199522743460ce76485acbb145f848
SeoJin Kim <sjinjin7@gmail.com>
2021-11-14 22:12:38 +0900

    장바구니 구현
    - 장바구니 페이지
    - 장바구니 페이지 이동(main.jsp, CartController)
    - 장바구니 필터(CartInterceptor.java)

---
+++ VamPa_MySQL/src/main/java/com/vam/controller/CartController.java
@@ -0,0 +1,18 @@
+package com.vam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class CartController {
+
+	/* 장바구니 페이지 이동 */
+	@GetMapping("/cart/{memberId}")
+	public String cartPageGET(@PathVariable("memberId") String memberId, Model model) {
+		model.addAttribute("member", memberId);
+		return "/cart";
+	}		
+	
+}
