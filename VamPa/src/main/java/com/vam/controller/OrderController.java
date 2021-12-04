f51442a903e42e97bb2e79cf4beb0d0918816928
SeoJin Kim <sjinjin7@gmail.com>
2021-12-05 00:44:22 +0900

    주문페이지 데이터 서버 넘기기

--- VamPa/src/main/java/com/vam/controller/OrderController.java
+++ VamPa/src/main/java/com/vam/controller/OrderController.java
@@ -1,15 +1,14 @@
 package com.vam.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.vam.model.MemberVO;
 import com.vam.model.OrderRequestWrapper;
 import com.vam.service.MemberService;
 import com.vam.service.OrderService;
@@ -37,12 +36,13 @@ public class OrderController {
 		return "/order";
 	}
 	
-	
+	@PostMapping("/order")
+	public String orderPagePost(OrderRequestWrapper orw, HttpServletRequest request) {
+		System.out.println(orw);
+		return "redirect:/main";
+	}
 
 	
-	/* 사용자 정보 */
-	
-	/* 상품 정보 */
 
 	
 	
--- VamPa/src/main/java/com/vam/model/OrderRequestDTO.java
+++ VamPa/src/main/java/com/vam/model/OrderRequestDTO.java
@@ -5,6 +5,23 @@ public class OrderRequestDTO {
     private int bookId;
     
     private int bookCount;
+    
+    private int orderItemId;
+    
+    private int orderId;
+    
+    private int bookPrice;
+    
+    private int bookDiscount;
+    
+    private int savePoint;
+    
+    // tabe x
+    private int salePrice;
+    
+    private int totalPrice;
+    
+    private int totalSavePoint;
 
 	public int getBookId() {
 		return bookId;
@@ -22,10 +39,71 @@ public class OrderRequestDTO {
 		this.bookCount = bookCount;
 	}
 
+	public int getOrderItemId() {
+		return orderItemId;
+	}
+
+	public void setOrderItemId(int orderItemId) {
+		this.orderItemId = orderItemId;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	public int getBookDiscount() {
+		return bookDiscount;
+	}
+
+	public void setBookDiscount(int bookDiscount) {
+		this.bookDiscount = bookDiscount;
+	}
+
+	public int getSavePoint() {
+		return savePoint;
+	}
+
+	public int getSalePrice() {
+		return salePrice;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public int getTotalSavePoint() {
+		return totalSavePoint;
+	}
+	
+	public void initSaleTotal() {
+		this.salePrice = (int) (this.bookPrice * (1-this.bookDiscount));
+		this.totalPrice = this.salePrice*this.bookCount;
+		this.savePoint = (int)(Math.floor(this.salePrice*0.05));
+		this.totalSavePoint =this.savePoint * this.bookCount;
+		
+	}	
+
 	@Override
 	public String toString() {
-		return "OrderRequestDTO [bookId=" + bookId + ", bookCount=" + bookCount + "]";
-	}
+		return "OrderRequestDTO [bookId=" + bookId + ", bookCount=" + bookCount + ", orderItemId=" + orderItemId
+				+ ", orderId=" + orderId + ", bookPrice=" + bookPrice + ", bookDiscount=" + bookDiscount
+				+ ", savePoint=" + savePoint + ", salePrice=" + salePrice + ", totalPrice=" + totalPrice
+				+ ", totalSavePoint=" + totalSavePoint + "]";
+	}    
+
+
     
     
 	
